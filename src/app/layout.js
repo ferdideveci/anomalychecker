a51198d9ac5fa3897693b05536820918a3a8e9c2
ferdideveci <ferdideveci101@gmail.com>
2023-11-19 17:49:22 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <Analytics />
+      </body>
     </html>
   )
 }
