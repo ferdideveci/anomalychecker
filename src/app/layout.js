5873f73d58e5e13137f982e4065c50b8956f09bf
ferdideveci <ferdideveci101@gmail.com>
2023-11-19 17:47:28 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import'./page.module.css'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
