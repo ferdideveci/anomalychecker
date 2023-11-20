9ca51dd22b250f8f48e34a73c35062e01984ebb7
ferdideveci <ferdideveci101@gmail.com>
2023-11-20 22:44:52 +0100

    test

--- pages/layout.js
+++ pages/layout.js
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
-import '../styles/globals.css'
-import'./page.module.css'
 import { metadata } from './metadata.js'
 
 export default function RootLayout({ children }) {
