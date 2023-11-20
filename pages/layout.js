9b89bbac97679125d8f35fe760427abf0b7f1c50
ferdideveci <ferdideveci101@gmail.com>
2023-11-20 22:43:57 +0100

    test

--- pages/layout.js
+++ pages/layout.js
@@ -2,18 +2,16 @@ import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
 import'./page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Anomalychecker.app - Track any set of the Anomaly AI NFT collection',
-  description: 'Track any set of the Anomaly AI NFT collection. Developed by @fxru_eth',
-}
+import { metadata } from './metadata.js'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* other meta tags as needed */}
+      </head>
       <body className={inter.className}>{children}
       <Analytics />
       </body>
---
+++ pages/metadata.js
@@ -0,0 +1,5 @@
+// metadata.js
+export const metadata = {
+    title: 'Anomalychecker.app - Track any set of the Anomaly AI NFT collection',
+    description: 'Track any set of the Anomaly AI NFT collection. Developed by @fxru_eth',
+  }
